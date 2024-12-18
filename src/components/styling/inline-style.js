1d3a73b932da03963b729025bff0546c76c7c976
BallamPravallika <142523420+BallamPravallika@users.noreply.github.com>
2024-12-18 12:43:07 +0530

    seventeen

---
+++ src/components/styling/inline-style.js
@@ -0,0 +1,16 @@
+const Inlinestyle=()=>{
+    const headingstyle={
+        color:"Green",
+        fontsize:10,
+        backgroundcolor:"yello"
+        
+    }
+    return(
+        <h4 className={"geeting"}>Goodafternoon</h4>
+    
+    );
+
+
+}
+export default Inlinestyle
+//inline styles or styles objects
