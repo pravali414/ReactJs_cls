2b7404c8f1ebb6f36f36f16264a3cf8e11aa2f84
BallamPravallika <142523420+BallamPravallika@users.noreply.github.com>
2024-12-29 10:35:34 +0530

    usememo,usereducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import UseEffectEx1 from "./UseEffectEx1"; // Adjust the path if needed
+
+
+
+
 
 const App = () => {
   return (
     <div>
-      <UseEffectEx1 />
+   <ParentMemo/>
     </div>
   );
 };
--- src/screens/postLoginScreen/settingScreen.js
+++ src/screens/postLoginScreen/settingScreen.js
@@ -1,12 +1,22 @@
+import React, { useState } from "react";
+import ParentMemo from "../../components/reactMemo/parent";
+import NavBar from "../../navbar/navBar";
+import Parent from "../../components/reactMemo/parent";
+import ParentusemEMO from "../../components/hooks/useMemo/parentusemEMO.JS";
 
 
-const SettingScreen=()=>{
 
-
-    return(
+const SettingScreen = () => {
+    const [count, setCount] = useState(0); // Example state variable
+    
+    return (
         <>
-        <h2> I am from SettingScreen</h2>
+            <NavBar/>
+            <ParentusemEMO/>
+            
+            
+            <h2> I am from SettingScreen</h2>
         </>
-    )
-}
+    );
+};
 export default SettingScreen;
